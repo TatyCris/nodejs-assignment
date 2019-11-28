90ddc4d488843c5ae0b7c2175473b1bd0a3c9448
TatySilva <tatyccsilva@gmail.com>
2019-11-28 22:28:41 +0100

    Update router to get-vehicleData-by-id controller

--- src/rest-api/routes/vehicleData.js
+++ src/rest-api/routes/vehicleData.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { getVehiclesData } = require('../controllers/get-vehiclesData')
+const { getVehicleDataById } = require('../controllers/get-vehicleData-by-id')
 
 const router = express.Router()
 
 router.get('/vehicleData', getVehiclesData)
+router.get('/:id', getVehicleDataById);
 
 module.exports = router
