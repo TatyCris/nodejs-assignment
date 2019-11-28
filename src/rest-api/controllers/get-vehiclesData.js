e631ac64a941982b06ed6b70f4698d07c480b9ae
TatySilva <tatyccsilva@gmail.com>
2019-11-28 22:16:21 +0100

    Create get-vehiclesData controller

---
+++ src/rest-api/controllers/get-vehiclesData.js
@@ -0,0 +1,12 @@
+const Vehicle = require('../schemas/vehicle')
+
+const getVehiclesData = async (req, res) => {
+    try {
+        const vehiclesData = await Vehicle.find()
+        res.json(vehiclesData)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+module.exports = { getVehiclesData }
