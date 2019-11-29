1a102b87a08287439bc09666a572aa379b1a27c8
TatySilva <tatyccsilva@gmail.com>
2019-11-29 22:31:31 +0100

    Create serve for test the rest-api

---
+++ src/rest-api/tests/server.js
@@ -0,0 +1,30 @@
+const cors = require('cors')
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('../../dataBase/mongo')
+const vehicleDataRoutes = require('./routes')
+// const dataByIdRoutes = require('./routes')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.status(200).send('API running!')
+})
+
+const port = process.env.PORT_REST_API_TEST || 8000
+db().then(async () => {
+    app.listen(port, (error) => {
+        if (error) {
+            throw new Error('Internal Server Error')
+        }
+        console.log(`Api-test listen on PORT: ${port}`)
+    })
+})
+
+app.use(vehicleDataRoutes)
+// app.use(dataByIdRoutes)
+
+module.exports = app
