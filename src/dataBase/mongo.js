7ebf59a6eaf1b7617110b57147ba39951acdbf48
TatySilva <tatyccsilva@gmail.com>
2019-11-29 10:30:02 +0100

    Update mongo dataBase url

--- src/dataBase/mongo.js
+++ src/dataBase/mongo.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const vehicle = require('../rest-api/schemas/vehicle')
 
 const db = () => {
-    return mongoose.connect(process.env.DATABASE_URL, { 
+    return mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017', { 
         useNewUrlParser: true,
         useUnifiedTopology: true 
     })
