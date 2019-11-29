90cc8186612279095cef5443335e1f9a52a0e4d9
TatySilva <tatyccsilva@gmail.com>
2019-11-29 15:41:38 +0100

    Create server for rest-api

--- src/rest-api/server.js
+++ src/rest-api/server.js
@@ -1,27 +1,25 @@
 const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
-const stream = require('./stream/nats')
-const db = require('./dataBase/mongo')
-const Vehicle = require('./dataBase/schemas/vehicle')
+const db = require('../dataBase/mongo')
+const vehicleDataRoutes = require('./routes/vehicleData')
 
 const app = express()
 app.use(cors())
-
-const jsonParser = bodyParser.json()
-app.use(jsonParser)
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.status(200).send('API running!')
 })
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 6000
 db().then(async () => {
     app.listen(port, (error) => {
         if (error) {
             throw new Error('Internal Server Error')
         }
-        stream(Vehicle)
         console.log(`Listen on PORT: ${port}`)
     })
 })
+
+app.use(vehicleDataRoutes)
