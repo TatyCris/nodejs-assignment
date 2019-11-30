1d43fff61eaae9e3775a54066139399957fd5f3f
TatySilva <tatyccsilva@gmail.com>
2019-11-30 01:08:11 +0100

    Create webSocket tests

---
+++ src/webSocket-api/tests/webSocket.test.js
@@ -0,0 +1,18 @@
+const WebSocket = require('ws')
+const VehicleModel = require('../../dataBase/tests/vehicleSchemaTest')
+
+describe('app', () => {
+    beforeAll(async () => {
+        VehicleModel.remove({}, (err) => { });
+    });
+
+    it('connect websockets response', async (done) => {
+        const url = 'http://localhost:9000'
+        const connection = new WebSocket(url)
+            .on('message', (msg) => {
+                expect(msg).toEqual(expect.anything());
+                connection.close();
+            })
+            .on('close', () => done());
+    });
+});
